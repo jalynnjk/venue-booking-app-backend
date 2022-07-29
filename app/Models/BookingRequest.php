fc13f6b630a8d59bbd607f5b8d362ece37265523
Jalynn <jalynnking@gmail.com>
2022-07-29 00:48:59 -0400

    Update model to allow for create route

--- app/Models/BookingRequest.php
+++ app/Models/BookingRequest.php
@@ -8,14 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class BookingRequest extends Model
 {
     use HasFactory;
-    protected $casts = [
-        'client_name' => 'string',
-        'client_email' => 'string',
-        'wedding_date' => 'datetime:d-m-Y',
-        'number_guests' => 'integer',
-        'budget' => 'integer',
-        'ceremony_location' => 'string',
-        'reception_location' => 'string',
-        'new_request' => 'boolean',
+    protected $fillable = [
+        'client_name',
+        'client_email',
+        'wedding_date',
+        'number_guests',
+        'budget',
+        'ceremony_location',
+        'reception_location',
+        'new_request',
     ];
 };
