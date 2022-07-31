2eaad12ac8a0c046b098afc717c532c7b2a2a766
Jalynn <jalynnking@gmail.com>
2022-07-31 00:25:40 -0400

    Create table for accepted booking requests

---
+++ database/migrations/2022_07_31_040622_create_acceptedrequests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acceptedrequests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('client_name');
+            $table->string('client_email');
+            $table->date('wedding_date');
+            $table->integer('budget');
+            $table->integer('number_guests');
+            $table->string('ceremony_location');
+            $table->string('reception_location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acceptedrequests');
+    }
+};
