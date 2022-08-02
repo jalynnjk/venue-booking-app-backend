de7e2f957923f85e8bb747a7a0af547521d4416a
Jalynn <jalynnking@gmail.com>
2022-08-01 20:16:51 -0400

    Still trying to fix config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-$DATABASE_URL = parse_url(env('DATABASE_URL'));
 
 return [
 
@@ -66,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL,
-            'host' => $DATABASE_URL("host"),
-            'port' => $DATABASE_URL("port"),
-            'database' => ltrim($DATABASE_URL["path"]),
-            'username' => $DATABASE_URL("user"),
-            'password' => $DATABASE_URL("pass"),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => '',
